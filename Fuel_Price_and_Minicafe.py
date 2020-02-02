296a89fdd763de2c22759dab0dbe7a3d778067ba
ElaySuperCoder <36122259+Maharramli@users.noreply.github.com>
2020-02-02 18:19:42 +0400

    minicafe calculate corrected

--- Fuel_Price_and_Minicafe.py
+++ Fuel_Price_and_Minicafe.py
@@ -1,6 +1,12 @@
 from tkinter import *
 
 tk = Tk()
+coke = 2
+burg = 2
+fri = 4
+cola = 1
+cizburg = 3
+
 while True:
     tk.configure(background="light blue")
     tk.maxsize(900,700)
@@ -10,25 +16,26 @@ while True:
     title = Label(text="Benzin", font=("Courier", 40), bg="light blue")
     def cal():
         if str(aznbox.get()) == "":
-            hesabfuel.configure(text="Zəhmət olmasa etibarlı bir dəyər daxil edin")
+            hesabfuel.configure(text="Etibarlı bir dəyər daxil edin")
         else:
             hesabfuel.configure(text=str(round(int(aznbox.get()) / litrfuel,2))+ " Litr")
+    def caltwo():
+        if burgent.get() or cizburgent.get() or frient.get() or cokent.get() or colaent.get() == "":
+            hesabkafe.configure(text="Etibarlı bir dəyər daxil edin")
+        total = (int(burgent.get()) * burg) + (int(cizburgent.get()) * cizburg) + (int(frient.get()) * fri) + (int(cokent.get()) * coke) + (int(colaent.get()) * cola)
+        hesabkafe.configure(text=str(total) + str(" AZN"))
     azn = Label(text="AZN:", font=("Arial", 17), bg="light blue")
     aznbox = Entry()
     button = Button(text="Hesabla", command=cal,bg="grey")
-    button2 = Button(text="Hesabla",bg="blue")
+    button2 = Button(text="Hesabla",bg="blue",command=caltwo)
     title2 = Label(text="Minikafe",bg="light blue",font = ("Courier",40))
     hesabfuel = Label(text="",bg="light blue",font=("Arial", 17))
+    hesabkafe = Label(text="",bg="light blue", font=("Arial", 17))
     burgent = Entry()
     frient = Entry()
     cizburgent = Entry()
     cokent = Entry()
     colaent = Entry()
-    burgent.insert(0, "Say daxil edin")
-    frient.insert(0, "Say daxil edin")
-    cizburgent.insert(0, "Say daxil edin")
-    cokent.insert(0, "Say daxil edin")
-    colaent.insert(0, "Say daxil edin")
     drinktxt = Label(text="İçkilər", bg="light blue", font=("Arial", 30))
     burgtxt = Label(text="Hamburger:2 AZN",bg="light blue",font = ("Arial",17))
     fritxt = Label(text="Fri:4 AZN",bg="light blue",font=("Arial", 16))
@@ -36,8 +43,6 @@ while True:
     coketxt = Label(text="Kokteyl:2 AZN", bg="light blue", font=("Arial", 16))
     colatxt = Label(text="Coca-Cola:1 AZN", bg="light blue", font=("Arial", 16))
     litrfuel = 1.50
-    burger = 2
-    fri = 3
     title.place(x=100, y=10)
     title2.place(x=450, y=10)
     azn.place(x = 100,y = 90)
@@ -51,6 +56,7 @@ while True:
     coketxt.place(x = 450,y = 320)
     colatxt.place(x = 450,y = 365)
     hesabfuel.place(x = 100,y = 400)
+    hesabkafe.place(x = 450, y = 500)
     burgent.place(x = 685,y = 107)
     frient.place(x = 578,y = 156)
     cizburgent.place(x = 665,y = 206)
